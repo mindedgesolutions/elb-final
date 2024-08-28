002b96e8bed99d006e3e64d3e4325cc3b8573da1
Souvik Nag <s.nag26@gmail.com>
2024-08-28 22:34:22 +0530

    top nav login btn

--- frontend/src/components/website/globals/WbTopnav.jsx
+++ frontend/src/components/website/globals/WbTopnav.jsx
@@ -7,8 +7,11 @@ import {
 import logo from "@/assets/website/img/logo/logo-white.svg";
 import { Link } from "react-router-dom";
 import { IndianRupee, User } from "lucide-react";
+import { useSelector } from "react-redux";
 
 const WbTopnav = () => {
+  const { currentUser } = useSelector((store) => store.currentUser);
+
   return (
     <header className="header-primary">
       <div className="container">
@@ -19,22 +22,25 @@ const WbTopnav = () => {
           <WbTopSearch />
           <div className="navbar-right d-flex align-items-center gap-4">
             <div className="align-items-center d-none d-lg-flex">
-              <Link
-                to="/sign-in"
-                className="w-btn-secondary-lg border font-medium border-white px-6 py-3 text-md w-32"
-              >
-                <User size={18} className="font-medium" />
-                Login
-              </Link>
-              <Link to={`#`}>
-                <button
-                  type="button"
-                  className="w-btn-secondary-lg border font-medium border-white px-6 py-3 text-md"
+              {currentUser ? (
+                <Link to={`#`}>
+                  <button
+                    type="button"
+                    className="w-btn-secondary-lg border font-medium border-white px-6 py-3 text-md"
+                  >
+                    <IndianRupee size={16} className="font-medium" />
+                    Post Ad
+                  </button>
+                </Link>
+              ) : (
+                <Link
+                  to="/sign-in"
+                  className="w-btn-secondary-lg border font-medium border-white px-6 py-3 text-md w-32"
                 >
-                  <IndianRupee size={16} className="font-medium" />
-                  Post Ad
-                </button>
-              </Link>
+                  <User size={18} className="font-medium" />
+                  Login
+                </Link>
+              )}
             </div>
             <TopNavProfileDropdown />
 
